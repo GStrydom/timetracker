5c186fd25c2e5fa67501210367dc163cb68a5579
Gregory <gregory.strydom079@gmail.com>
2021-08-30 06:22:08 +0200

    Include transfer service

--- src/app/components/timesheet/new-timesheet/new-timesheet.component.ts
+++ src/app/components/timesheet/new-timesheet/new-timesheet.component.ts
@@ -6,6 +6,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 
 import 'rxjs/add/operator/map';
+import {TransferService} from '../../shared/transfer.service';
 
 @Component({
   selector: 'app-new-timesheet',
@@ -15,20 +16,26 @@ import 'rxjs/add/operator/map';
 export class NewTimesheetComponent implements OnInit {
   timesheets: Observable<any>;
   formObj = {};
+  selectedValue: string;
 
-  constructor(private timesheetService: TimesheetService, private db: AngularFirestore) { }
+  constructor(private timesheetService: TimesheetService, private db: AngularFirestore, private transferService: TransferService) { }
+    tasks: any[] = [
+      { value: 'Connect Testing', viewValue: 'Connect Testing' },
+      { value: 'Immerse Testing', viewValue: 'Immerse Testing' },
+      { value: 'Break', viewValue: 'Break' },
+    ];
 
   ngOnInit(): void {
   }
 
-  onSubmit(form: NgForm) {
+  onSubmit(form: NgForm): any {
     this.formObj = {
       date: form.value.date,
       startTime: form.value.startTime,
       taskDescription: form.value.taskDescription,
       endTime: form.value.endTime,
-      day: 'Monday',
-      hoursWorked: 8
+      day: this.transferService.getData(),
+      hoursWorked: form.value.hours
     };
     console.log(this.formObj);
     this.timesheetService.addTimesheetToFirestore(this.formObj);
