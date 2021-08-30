0f54e60b3886822c184de1514b36d902e32b1d08
Gregory <gregory.strydom079@gmail.com>
2021-08-30 06:19:53 +0200

    Import xlsx for timesheet exports

--- src/app/components/weekdays/monday/monday.component.ts
+++ src/app/components/weekdays/monday/monday.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Timesheet } from '../../timesheet/timesheet.model';
 import { TimesheetService } from '../../timesheet/timesheet.service';
+import { Totals } from '../../timesheet/totals.models';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-monday',
@@ -9,14 +11,36 @@ import { TimesheetService } from '../../timesheet/timesheet.service';
   styleUrls: ['./monday.component.css']
 })
 export class MondayComponent implements OnInit {
-  displayedColumns = ['date', 'startTime', 'taskDescription', 'endTime', 'hoursWorked'];
-  datasource: MatTableDataSource<Timesheet>;
+  mainColumns = ['date', 'startTime', 'taskDescription', 'endTime', 'hoursWorked'];
+  totalColumns = ['totalHours', 'amount'];
+  mainDatasource: MatTableDataSource<Timesheet>;
+  totalDatasource: MatTableDataSource<Totals>;
+  daysTimesheet = [];
+  totals = [];
+  @ViewChild('TABLE') table: ElementRef;
 
   constructor(private timesheetService: TimesheetService) { }
 
   ngOnInit(): void {
-    this.timesheetService.getMondayTimesheets().subscribe(result => {
-      this.datasource = new MatTableDataSource(result);
+    this.totals.push({totalHours: 40, amount: 'R2,160.00'});
+
+    this.timesheetService.getUserTimesheets().subscribe(result => {
+      for (let x = 0; x < result.length; x++) {
+        if (result[x].day === 'Monday') {
+          this.daysTimesheet.push(result[x]);
+        }
+      }
+      this.mainDatasource = new MatTableDataSource(this.daysTimesheet);
+      this.totalDatasource = new MatTableDataSource(this.totals);
     });
+  }
+
+  exportAsExcel(): any {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'Timesheet.xlsx');
   }
 }
