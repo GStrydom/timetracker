28408603f1d17c541a6b87f31dc11b147288cbee
Gregory <gregory.strydom079@gmail.com>
2021-08-30 06:16:44 +0200

    Numerous updates

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,4 +1,3 @@
-import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs/Subject';
 import { Injectable } from '@angular/core';
@@ -6,22 +5,25 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UIService } from '../shared/ui/ui.service';
+import {TransferService} from '../shared/transfer.service';
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private isAuthenticated = false;
 
-  constructor(private router: Router, private auth: AngularFireAuth, public snackBar: MatSnackBar, private uiService: UIService) {}
+  constructor(private router: Router, private auth: AngularFireAuth, public snackBar: MatSnackBar, private uiService: UIService, private transferService: TransferService) {}
 
-  registerUser(authData: AuthData) {
+  registerUser(authData: AuthData): any {
     this.uiService.loadingStateChanged.next(true);
     this.auth.createUserWithEmailAndPassword(authData.email, authData.password)
       .then(result => {
         this.uiService.loadingStateChanged.next(false);
-        console.log(result);
         this.authChange.next(true);
         this.router.navigate(['/home']).then();
+        result.user.updateProfile({
+          displayName: authData.displayName
+        }).then();
       }).catch(error => {
         this.uiService.loadingStateChanged.next(false);
         console.log(error.message);
@@ -29,14 +31,15 @@ export class AuthService {
     });
   }
 
-  login(authData: AuthData) {
+  login(authData: AuthData): any {
     this.uiService.loadingStateChanged.next(true);
     this.auth.signInWithEmailAndPassword(authData.email, authData.password)
       .then(result => {
         this.uiService.loadingStateChanged.next(false);
-        console.log(result);
         this.snackBar.open('Login success! Welcome back :)', null, {duration: 3000});
         this.isAuthenticated = true;
+        localStorage.setItem('userName', result.user.displayName);
+        localStorage.setItem('userEmail', authData.email);
         this.authChange.next(true);
         this.router.navigate(['/home']).then();
     }).catch(error => {
@@ -46,7 +49,7 @@ export class AuthService {
     });
   }
 
-  logout() {
+  logout(): any {
     this.auth.signOut().then();
     this.snackBar.open('You have been logged out. See you later!', null, {duration: 3000});
     this.isAuthenticated = false;
@@ -54,11 +57,7 @@ export class AuthService {
     this.router.navigate(['/login']).then();
   }
 
-  getUser() {
-
-  }
-
-  isAuth() {
+  isAuth(): any {
     return this.isAuthenticated;
   }
 }
