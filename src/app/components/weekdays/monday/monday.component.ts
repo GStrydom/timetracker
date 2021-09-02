a832554a5cfccdf3c75a1a14d363165399efedef
Gregory <gregory.strydom079@gmail.com>
2021-09-02 08:48:04 +0200

    Lots of updates

--- src/app/components/weekdays/monday/monday.component.ts
+++ src/app/components/weekdays/monday/monday.component.ts
@@ -8,6 +8,7 @@ import { Totals } from '../../timesheet/totals.models';
 import * as XLSX from 'xlsx';
 import { Router } from '@angular/router';
 import { TransferService } from '../../shared/transfer.service';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
 
 import '../../../../assets/smtp.js';
 declare let Email: any;
@@ -32,10 +33,13 @@ export class MondayComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor(private timesheetService: TimesheetService, private router: Router, private transferService: TransferService) { }
+  constructor(private timesheetService: TimesheetService, private router: Router, private transferService: TransferService, private fs: AngularFirestore) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<any> {
     this.timesheetService.getUserTimesheets().subscribe(result => {
+      if (result.length <= 0) {
+        this.transferService.setData('true');
+      }
       for (let x = 0; x < result.length; x++) {
         if (result[x].day === 'Monday') {
           // tslint:disable-next-line:radix
@@ -57,39 +61,55 @@ export class MondayComponent implements OnInit {
     });
   }
 
-  exportAsExcel(): any {
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  async exportAsExcel(): Promise<any> {
+    if (this.checkTable('timesheet')) {
+      const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
-    /* save to file */
-    XLSX.writeFile(wb, 'Timesheet.xlsx');
+      /* save to file */
+      XLSX.writeFile(wb, 'Timesheet.xlsx');
+    }
   }
 
-  emailToAdmin(): any {
-    Email.send({
-      Host: 'smtp.elasticemail.com',
-      Username: 'gregstrydom5@gmail.com',
-      Password: 'BFF36BE0713DCAF828E4C2CD48F77B968528',
-      To : 'gregory.strydom079@gmail.com',
-      From : 'gregstrydom5@gmail.com',
-      Subject : 'This is the subject',
-      Body : 'And this is the body',
-      Attachments : [
-        {
+  async emailToAdmin(): Promise<any> {
+    if (this.checkTable('email')) {
+      Email.send({
+        Host: 'smtp.elasticemail.com',
+        Username: 'gregstrydom5@gmail.com',
+        Password: 'BFF36BE0713DCAF828E4C2CD48F77B968528',
+        To : 'gregory.strydom079@gmail.com',
+        From : 'gregstrydom5@gmail.com',
+        Subject : 'This is the subject',
+        Body : 'And this is the body',
+        Attachments : [
+          {
 
-        }]
-    }).then(
-      message => alert(message)
-    );
+          }]
+      }).then(
+        message => alert(message)
+      );
+    }
   }
 
-  editRecord(element): any {
+  async editRecord(element): Promise<any> {
     this.transferService.setData(element);
     this.router.navigate(['edit-timesheet']).then();
   }
 
-  deleteRecord(element): any {
-    console.log(element);
+  async deleteRecord(element): Promise<any> {
+    const answer = confirm('Are you sure you would like to delete this record?');
+    if (answer) {
+      this.fs.collection('finishedTimesheets').doc(element.id).delete().then();
+    }
+  }
+
+  checkTable(method): any {
+    const temp = this.transferService.getData();
+    if (temp === 'true') {
+      alert('Please create a {{ method }} before trying export.');
+    } else {
+      return true;
+    }
   }
 }
