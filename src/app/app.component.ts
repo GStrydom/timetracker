78372123c5e5b25e2b4d33538281aa61b6a6c4bb
Gregory <gregory.strydom079@gmail.com>
2021-09-02 08:40:02 +0200

    Implement auth listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './components/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private authService: AuthService) {}
   sideNavOpened = false;
 
-  sideOpen() {
+  sideOpen(): any {
 
+  }
+
+  ngOnInit(): void {
+    this.authService.initAuthListener();
   }
 }
