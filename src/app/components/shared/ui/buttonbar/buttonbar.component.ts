b7bbeba6cb73752ec2680069fb1c6388dcaa8dc7
Gregory <gregory.strydom079@gmail.com>
2021-09-02 08:42:50 +0200

    Add checktable method

--- src/app/components/shared/ui/buttonbar/buttonbar.component.ts
+++ src/app/components/shared/ui/buttonbar/buttonbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TransferService} from '../../transfer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buttonbar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private transferService: TransferService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  checkTable(): any {
+    const temp = this.transferService.getData();
+    if (temp === 'true') {
+      alert('Please create a timesheet before trying to add a record.');
+    } else {
+      this.router.navigate(['/new-timesheet']).then();
+    }
   }
 
 }
