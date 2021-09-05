79f546e2439a9e888a55f5bfef260473d6b3df0b
Gregory <gregory.strydom079@gmail.com>
2021-09-05 06:27:39 +0200

    Include profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,14 @@ import { RegisterComponent } from './components/auth/register/register.component
 import {EditTimesheetComponent} from './components/timesheet/edit-timesheet/edit-timesheet.component';
 import {CreateTimesheetComponent} from './components/timesheet/create-timesheet/create-timesheet.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import {ProfileComponent} from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
   { path: 'welcome', component: WelcomeComponent, canActivate: [AuthGuard]},
   { path: 'new-timesheet', component: NewTimesheetComponent, canActivate: [AuthGuard] },
   { path: 'edit-timesheet', component: EditTimesheetComponent, canActivate: [AuthGuard] },
